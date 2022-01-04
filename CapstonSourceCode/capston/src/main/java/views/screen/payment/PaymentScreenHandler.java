d27600da04a8d6821cc22d3a5264a6c812d1dd7c
tranngocphien <tranngocphien0309@gmail.com>
2022-01-05 00:01:54 +0700

    update back previous screen

--- CapstonSourceCode/capston/src/main/java/views/screen/payment/PaymentScreenHandler.java
+++ CapstonSourceCode/capston/src/main/java/views/screen/payment/PaymentScreenHandler.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import utils.Configs;
 import views.screen.BaseScreenHandler;
@@ -98,5 +99,11 @@ public class PaymentScreenHandler extends BaseScreenHandler {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    void back(MouseEvent event) {
+        getPreviousScreen().show();
+
     }
 }
